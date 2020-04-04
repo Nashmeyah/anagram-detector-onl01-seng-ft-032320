764567236f2cda5b8993a3504fdd6bc9268e79e7
Nashmeyah Al-Rekabi <nashalrekabi@gmail.com>
2020-04-04 16:51:46 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,11 +1,17 @@
 class Anagram
-  attr_accessor :anagram
+  attr_accessor :anagram #a similar word consisting of the same letters
 
   def initialize(word)
-
+    @word = word
   end
 
   def match(possible_matches)
 
   end
 end
+
+#takes a list of anagrams
+#split the list
+#itterate through new array of possible anagrams
+#match word with list of anagram that returns true or false
+
